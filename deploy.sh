0787978dc0800755083554240acb7d35277633f2
modood <modood@qq.com>
2016-10-10 13:46:39 +0800

    Install npm dependencies

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# check git and npm
+(command -v git >/dev/null 2>&1 && command -v npm >/dev/null 2>&1) || {
+  echo >&2 "You first need to have git and npm installed. Aborting.";
+  exit 1;
+}
+
 vundle=~/.vim/bundle/Vundle.vim
 
 if [ ! -d "$vundle" ]; then
@@ -10,3 +16,8 @@ cp .vimrc ~/
 vim +PluginInstall +qall
 
 cp -rf snippets/* ~/.vim/bundle/snipmate.vim/snippets
+
+# install npm dependencies
+command -v eslint >/dev/null 2>&1 || sudo npm install -g eslint
+command -v instant-markdown-d >/dev/null 2>&1 || sudo npm install -g instant-markdown-d
+
